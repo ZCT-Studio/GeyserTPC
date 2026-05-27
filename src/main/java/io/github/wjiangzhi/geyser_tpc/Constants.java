4641cc6be7eea48ad95dc325f764a7041c7e40dc
wanjiangzhi <wanjiangzhi@outlook.com>
2026-05-27 16:22:41 +0800

    fix version

--- src/main/java/io/github/wjiangzhi/geyser_tpc/Constants.java
+++ src/main/java/io/github/wjiangzhi/geyser_tpc/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Constants {
     public static final String MOD_ID = "geyser_tpc";
     public static final String MOD_NAME = "GeyserTPC";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
